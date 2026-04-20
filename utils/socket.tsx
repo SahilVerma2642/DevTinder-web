702accd0c6723b58652e115b22f34d2135629bc0
verma2604 <shailverma@valueresearch.in>
2026-04-20 09:33:56 +0530

    fix: resolve socket.emit TypeError by exporting socket instance instead of factory function

--- utils/socket.tsx
+++ utils/socket.tsx
@@ -1,6 +1,8 @@
 import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
-const socket = io(BASE_URL, { withCredentials: true });
+const socket = location.hostname === "localhost"
+  ? io(BASE_URL, { withCredentials: true })
+  : io("/", { path: "/api/socket.io", withCredentials: true });
 
 export default socket;
